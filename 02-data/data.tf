71019a18617125c37317b544b7b1e6a612b4cc73
Ganesh <ganeshvelugula28@gmail.com>
2023-11-23 15:04:10 +0530

    learn

---
+++ 02-data/data.tf
@@ -0,0 +1,5 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "^myami-\\d{3}"
+  owners           = ["self"]
+}
---
+++ 03-variable/var.tf
@@ -0,0 +1,9 @@
+variable "sample"{
+    default="hello world"
+}
+output "sampvar"{
+    data=var.sample
+}
+
+# List data type
+# map data type
