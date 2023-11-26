cf6afb63f37513d97b565678826b88f8061edc11
Ganesh <ganeshvelugula28@gmail.com>
2023-11-26 21:05:34 +0530

    resource

--- 02-data/data.tf
+++ 02-data/data.tf
@@ -7,3 +7,13 @@ data "aws_ami" "example" {
 output "ami_id" {
   value = data.aws_ami.example.id
 }
+
+
+resource "aws_instance" "ec2" {
+  ami= data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }  
+}
