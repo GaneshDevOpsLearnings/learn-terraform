c3c86de83560938c436dfc8520ccc77dd5c1f158
Ganesh <ganeshvelugula28@gmail.com>
2023-11-26 21:24:40 +0530

    modules

--- 04-module/sg/main.tf
+++ 04-module/sg/main.tf
@@ -1,3 +1,28 @@
-resource "aws_security_group" "name" {
-  
+resource "aws_security_group" "allow_tls" {
+  name        = "sg_terraform"
+  description = "terraform resource"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "ssh"
+    cidr_blocks      = ["0.0.0.0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+output "sg_id" {
+    value = aws_security_group.allow_tls
 }
